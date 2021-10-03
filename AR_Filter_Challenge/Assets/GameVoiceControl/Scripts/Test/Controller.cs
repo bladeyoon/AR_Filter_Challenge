b72449ace07713000fe23a0581dab71ecb074053
bladeyoon <duyoungyoon@gmail.com>
2021-10-03 13:35:49 -0700

    BasicVoiceCommandImplemented

--- AR_Filter_Challenge/Assets/GameVoiceControl/Scripts/Test/Controller.cs
+++ AR_Filter_Challenge/Assets/GameVoiceControl/Scripts/Test/Controller.cs
@@ -2,60 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour 
+{
+    public GameObject _nose;
+    public GameObject _eye;
+    public GameObject _mouth;
+    public GameObject _ear;
+    //public GameObject _face;
+
 
 	// Use this for initialization
-	void Start ( ) {
-		
-	}
+	void Start ( ) 
+    {
+        _nose = GameObject.Find("Panda_Nose");
+        _eye = GameObject.Find("Panda_Eyes");
+        _mouth = GameObject.Find("Panda_Mouth");
+        _ear = GameObject.Find("Panda_Ears");
+        //_face = GameObject.Find("Panda_Face");
 
-    enum Around {
-        stop,
-        up,
-        down,
-        left,
-        right
+        _nose.SetActive(false);
+        //_face.SetActive(false);
+        //_ear.SetActive(false);
+        _mouth.SetActive(false);
+        _eye.SetActive(false);
+
     }
 
-    public string CmdRotateToTheLeft = "rotate to the left";
-    public string CmdRotateToTheRight = "rotate to the right";
-    public string CmdRotateUp = "rotate up";
-    public string CmdRotateDown = "rotate down";
-    public string CmdStop = "stop";
+    enum Around 
+    {
+        face,
+        nose,
+        eye,
+        ear,
+        mouth
+    }
 
-    private Around rotateTo = Around.stop;
+    public string CmdHowToSayNose = "nose";
+    public string CmdHowToSayEye = "eye";
+    public string CmdHowToSayMouth = "mouth";
+    public string CmdHowToSayEar = "ear";
+    public string CmdHowToSayFace = "face";
 
-    private float _speed = 0.5f;
+    private Around TurnOn = Around.face;
+
+    //private float _speed = 0.5f;
 	
 	// Update is called once per frame
-	void Update ( ) {
-        switch ( rotateTo ) {
-            case Around.left:
-                this.transform.Rotate( Vector3.up, _speed );
+	void Update ( ) 
+    {
+        switch ( TurnOn ) 
+        {
+            case Around.ear:
+                _ear.transform.Rotate(Vector3.up, 5);
+                Debug.Log("Ears on!");
                 break;
-            case Around.right:
-                this.transform.Rotate( Vector3.down, _speed );
+            case Around.face:
+                //_face.SetActive(true);
+                Debug.Log("Face on!");
                 break;
-            case Around.down:
-                this.transform.Rotate( Vector3.left, _speed );
+            case Around.eye:
+                _eye.SetActive(true);
+                Debug.Log("Eyes on!");
                 break;
-            case Around.up:
-                this.transform.Rotate( Vector3.right, _speed );
+            case Around.nose:
+                _nose.SetActive(true);
+                Debug.Log("Nose on!");
+                break;
+            case Around.mouth:
+                _mouth.SetActive(true);
+                Debug.Log("Mouth on!");
                 break;
         }
 	}
 
-    public void onReceiveRecognitionResult( string result ) {
-        if ( result.Contains( CmdRotateUp ) )
-            rotateTo = Around.up;
-        if ( result.Contains( CmdRotateDown ) )
-            rotateTo = Around.down;
-        if ( result.Contains( CmdRotateToTheLeft ) )
-            rotateTo = Around.left;
-        if ( result.Contains( CmdRotateToTheRight ) )
-            rotateTo = Around.right;
-        if ( result.Contains( CmdStop ) )
-            rotateTo = Around.stop;
+    public void onReceiveRecognitionResult( string result )
+    {
+        if ( result.Contains(CmdHowToSayNose) )
+            TurnOn = Around.nose;
+        if ( result.Contains(CmdHowToSayEye) )
+            TurnOn = Around.eye;
+        if ( result.Contains(CmdHowToSayMouth) )
+            TurnOn = Around.mouth;
+        if ( result.Contains(CmdHowToSayEar) )
+            TurnOn = Around.ear;
+        if ( result.Contains(CmdHowToSayFace) )
+            TurnOn = Around.face;
     }
 
 }
